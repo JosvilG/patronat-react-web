88fdf4f9307809cb2478acd62b8a3d7b4b70cd54
JosvilG <josvilgprojects@gmail.com>
2025-01-17 16:47:30 +0100

    feat: New hook menu toggle

---
+++ src/hooks/useMenuToggle.jsx
@@ -0,0 +1,16 @@
+// hooks/useMenuToggle.js
+import { useState } from 'react'
+
+export const useMenuToggle = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev)
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
+
+  return { isOpen, toggleMenu, closeMenu }
+}
