95b9104c688b8263056f2c595477668865e1f259
JosvilG <josvilgprojects@gmail.com>
2025-01-20 16:43:08 +0100

    feat: Event page base template created

---
+++ src/pages/events/EventPage.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from './../../firebase/firebase'
+import tagColors from './../../models/tagColors'
+import { useTranslation } from 'react-i18next'
+
+const EventPage = () => {
+  const { id } = useParams()
+  const [event, setEvent] = useState(null)
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      try {
+        const eventDoc = await getDoc(doc(db, 'events', id))
+
+        if (eventDoc.exists()) {
+          setEvent(eventDoc.data())
+        } else {
+          console.log('Evento no encontrado')
+        }
+      } catch (error) {
+        console.error('Error obteniendo el evento: ', error)
+      }
+    }
+
+    fetchEvent()
+  }, [id])
+
+  if (!event) return <div>Cargando...</div>
+
+  return (
+    <div className="container p-6 mx-auto">
+      <h1 className="text-3xl font-semibold text-center text-gray-800">
+        {event.title}
+      </h1>
+
+      {event.imageURL && (
+        <img
+          src={event.imageURL}
+          alt={event.title}
+          className="object-cover w-full h-64 my-6 rounded-lg shadow-md"
+        />
+      )}
+
+      <p className="mb-6 text-lg text-gray-700">{event.description}</p>
+
+      <div className="mb-4">
+        <p>
+          <strong>{t('pages.events.details.startDate')}:</strong>{' '}
+          {new Date(event.startDate).toLocaleString()}
+        </p>
+        {event.endDate && (
+          <p>
+            <strong>{t('pages.events.details.endDate')}:</strong>{' '}
+            {new Date(event.endDate).toLocaleString()}
+          </p>
+        )}
+      </div>
+
+      <div className="mb-4">
+        <p>
+          <strong>{t('pages.events.details.startTime')}:</strong>{' '}
+          {event.startTime}
+        </p>
+        {event.endTime && (
+          <p>
+            <strong>{t('pages.events.details.endTime')}:</strong>{' '}
+            {event.endTime}
+          </p>
+        )}
+      </div>
+
+      <p className="mb-4">
+        <strong>{t('pages.events.details.location')}:</strong> {event.location}
+      </p>
+
+      <div className="mb-4">
+        <p>
+          <strong>{t('pages.events.details.capacity')}:</strong>{' '}
+          {event.capacity}
+        </p>
+        <p>
+          <strong>{t('pages.events.details.price')}:</strong> {event.price} €{' '}
+        </p>
+        <p>
+          <strong>{t('pages.events.details.minAge')}:</strong> {event.minAge}{' '}
+          años
+        </p>
+      </div>
+
+      {event.collaborators && event.collaborators.length > 0 && (
+        <div className="mb-4">
+          <strong>{t('pages.events.details.collaborators')}:</strong>
+          <ul className="ml-6 list-disc">
+            {event.collaborators.map((collaborator, index) => (
+              <li key={index} className="text-gray-700">
+                {collaborator}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {event.tags && event.tags.length > 0 && (
+        <div className="mb-4">
+          <strong>{t('pages.events.details.tags')}:</strong>
+          <ul className="flex space-x-4">
+            {event.tags.map((tag, index) => {
+              const tagColor = tagColors[tag] || 'bg-gray-600'
+              return (
+                <li
+                  key={index}
+                  className={`px-4 py-2 rounded-lg text-white ${tagColor}`}
+                >
+                  {tag}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
+
+      <div className="mb-4">
+        <p>
+          <strong>{t('pages.events.details.organizer')}:</strong>{' '}
+          {event.organizer}
+        </p>
+        <p>
+          <strong>{t('pages.events.details.allowCars')}:</strong>{' '}
+          {event.allowCars ? 'Sí' : 'No'}
+        </p>
+        <p>
+          <strong>{t('pages.events.details.hasBar')}:</strong>{' '}
+          {event.hasBar ? 'Sí' : 'No'}
+        </p>
+        <p>
+          <strong>{t('pages.events.details.status')}:</strong> {event.status}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default EventPage
