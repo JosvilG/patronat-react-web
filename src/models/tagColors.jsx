7067e04141e98937874e6748009cb978225d052f
JosvilG <josvilgprojects@gmail.com>
2025-01-20 16:41:46 +0100

    feat: New model for events color related with tags

---
+++ src/models/tagColors.jsx
@@ -0,0 +1,11 @@
+const tagColors = {
+  fmr: 'bg-red-100 text-red-800',
+  nadal: 'bg-blue-100 text-blue-800',
+  sants: 'bg-yellow-100 text-yellow-800',
+  raval: 'bg-green-100 text-green-800',
+  festival: 'bg-orange-100 text-orange-800',
+  reuniones: 'bg-purple-100 text-purple-800',
+  otros: 'bg-gray-100 text-gray-800',
+}
+
+export default tagColors
