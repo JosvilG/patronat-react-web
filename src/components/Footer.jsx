894bc83ee080bd2ba2768dacf91cbaf9e45efed5
JosvilG <josvilgprojects@gmail.com>
2025-01-29 16:19:19 +0100

    feat: Footer implemented

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="w-full h-[211px] bg-[#D9D9D9] text-black flex flex-col items-center relative">
+      <p className="absolute text-center t20sb top-10">
+        Carretera del Reguers, km 1, Roquetes
+        <br />
+        43520 Tarragona, España <br /> patronatfestesroquetes@gmail.com
+      </p>
+      <div className="absolute bottom-0 flex justify-between w-full direction-row">
+        <p className="text-black opacity-50 t14sb bottom-4 left-4">
+          © 2024 Patronat de Festes de Roquetes. <br />
+          Todos los derechos reservados.
+        </p>
+        <div className="text-end">
+          <p className="text-black opacity-50 t14sb bottom-4 left-4">
+            Web designed and developed by: Josep Vilchez Garcia
+          </p>
+          <p className="text-black opacity-50 t14sb bottom-4 left-4">
+            jvgcontacto@gmail.com or +34 650 851 990
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
