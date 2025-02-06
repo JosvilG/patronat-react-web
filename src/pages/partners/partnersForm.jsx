125943352f04a1b182c3fb5f3d4a7ce1804cc58d
JosvilG <josvilgprojects@gmail.com>
2025-02-06 10:02:39 +0100

    feat: Creating base partnersForm component

---
+++ src/pages/partners/partnersForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PartnersForm() {
+  return (
+    <div className="min-h-screen px-4 bg-transparent">
+      <section className="mt-[84px] flex flex-col">
+        <h2 className="t64s mb-[84px] text-end">Partner form</h2>
+      </section>
+    </div>
+  )
+}
+
+export default PartnersForm
