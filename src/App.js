d9d95bd1294cd49931d196d30dcf8efbdb7c166d
JosvilG <josvilgprojects@gmail.com>
2025-02-06 10:21:22 +0100

    refactor: Using routes component in app.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Auth from './pages/LoginPage'
 import Layout from './components/Layout'
-import RegisterPage from './pages/RegisterPage'
-import ProfilePage from './pages/ProfilePage'
 import ProtectedRoute from './components/ProtectedRoute'
-import Dashboard from './pages/Dashboard'
-import EventForm from './pages/events/EventRegister'
-import UploadGalleryForm from './pages/galery/uploadFiles'
-import CrewForm from './pages/crews/register-crew'
-import Settings from './pages/users/userSettings'
-import InputsShowcase from './pages/Test'
-import EventPage from './pages/events/EventPage'
-import FullEventsPage from './pages/events/FullEventsPage'
-import AboutPage from './pages/AboutPage'
 import UnderConstruction from './pages/UnderConstruction'
-
 import './translations/index'
-import StaffControl from './pages/users/staffControl'
-import PartnersForm from './pages/partners/partnersForm'
-
-const publicRoutes = [
-  { path: '/', element: <HomePage /> },
-  { path: '/login', element: <Auth /> },
-  { path: '/register', element: <RegisterPage /> },
-  { path: '/test', element: <InputsShowcase /> },
-  { path: '/events-list', element: <FullEventsPage /> },
-  { path: '/event/:eventName', element: <EventPage /> },
-  { path: '/about', element: <AboutPage /> },
-  { path: '/partner-form', element: <PartnersForm /> },
-]
-
-const protectedRoutes = [
-  { path: '/dashboard', element: <Dashboard /> },
-  { path: '/staff-control', element: <StaffControl /> },
-  { path: '/new-event', element: <EventForm /> },
-  { path: '/upload-file', element: <UploadGalleryForm /> },
-  { path: '/new-crew', element: <CrewForm /> },
-  { path: '/profile', element: <ProfilePage /> },
-  { path: '/settings', element: <Settings /> },
-]
+import { publicRoutes, protectedRoutes } from './routes/routes'
 
 function App() {
   return (
@@ -60,6 +24,8 @@ function App() {
               element={<ProtectedRoute>{element}</ProtectedRoute>}
             />
           ))}
+
+          {/* Página en construcción para rutas no encontradas */}
           <Route path="*" element={<UnderConstruction />} />
         </Route>
       </Routes>
