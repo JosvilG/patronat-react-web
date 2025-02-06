78ceffc0076dd00382fdeeddd55321f724df9260
JosvilG <josvilgprojects@gmail.com>
2025-02-06 09:41:13 +0100

    style: Footer skinned for mobile devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="w-full h-[211px] bg-[#D9D9D9] text-black flex flex-col items-center relative">
-      <p className="absolute text-center t20sb top-10">
+    <footer className="w-full h-[211px] bg-[#D9D9D9] max-sm:h-64 max-sm:mb-0 text-black flex flex-col items-center relative">
+      <p className="absolute text-center t20sb top-10 max-sm:top-6">
         Carretera del Reguers, km 1, Roquetes
         <br />
         43520 Tarragona, España <br /> patronatfestesroquetes@gmail.com
       </p>
-      <div className="absolute bottom-0 flex justify-between w-full direction-row">
-        <p className="text-black opacity-50 t14sb bottom-4 left-4">
+      <div className="absolute bottom-0 flex justify-between w-full px-4 direction-row max-sm:bottom-6">
+        <p className="text-black opacity-50 t12sb bottom-4 left-4">
           © 2024 Patronat de Festes de Roquetes. <br />
           Todos los derechos reservados.
         </p>
         <div className="text-end">
-          <p className="text-black opacity-50 t14sb bottom-4 left-4">
+          <p className="text-black opacity-50 t12sb bottom-4 left-4 ">
             Web designed and developed by: Josep Vilchez Garcia
           </p>
           <p className="text-black opacity-50 t14sb bottom-4 left-4">
