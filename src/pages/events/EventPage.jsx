fe42a7d4809f1457b4c1fa43ee67500ced747671
JosvilG <josvilgprojects@gmail.com>
2025-02-26 21:30:07 +0100

    refactor: Using Loader + DynamiCard + DynamicItems in event page details

--- src/pages/events/EventPage.jsx
+++ src/pages/events/EventPage.jsx
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from './../../firebase/firebase'
-import tagColors from './../../models/tagColors'
 import { useTranslation } from 'react-i18next'
+import Loader from '../../components/Loader'
+import DynamicCard from '../../components/Cards'
+import DynamicItems from '../../components/Items'
 
 const EventPage = () => {
   const { eventName } = useParams()
   const [event, setEvent] = useState(null)
+  const [loading, setLoading] = useState(true)
   const { t } = useTranslation()
   const navigate = useNavigate()
 
   useEffect(() => {
     const fetchEvent = async () => {
+      setLoading(true)
       try {
         const querySnapshot = await getDocs(collection(db, 'events'))
         const eventDoc = querySnapshot.docs.find(
@@ -28,122 +32,159 @@ const EventPage = () => {
         }
       } catch (error) {
         console.error('Error obteniendo el evento: ', error)
+      } finally {
+        setLoading(false)
       }
     }
 
     fetchEvent()
   }, [eventName, navigate])
 
-  if (!event) return <div>Cargando...</div>
+  if (loading) {
+    return <Loader loading={loading} />
+  }
+
+  if (!event) {
+    return (
+      <p className="text-center text-red-500">{t('pages.events.notFound')}</p>
+    )
+  }
+
+  const eventDataDetails = [
+    {
+      title: t('pages.events.details.startDate'),
+      description: new Date(event.startDate).toLocaleString(),
+      type: 'eventData',
+    },
+    event.endDate && {
+      title: t('pages.events.details.endDate'),
+      description: new Date(event.endDate).toLocaleString(),
+      type: 'eventData',
+    },
+    {
+      title: t('pages.events.details.startTime'),
+      type: 'eventData',
+      description: event.startTime,
+    },
+    event.endTime && {
+      title: t('pages.events.details.endTime'),
+      type: 'eventData',
+      description: event.endTime,
+    },
+  ].filter(Boolean)
+
+  const eventAccessDetails = [
+    {
+      title: t('pages.events.details.location'),
+      description: event.location,
+      type: 'eventData',
+    },
+
+    {
+      title: t('pages.events.details.capacity'),
+      description: event.capacity,
+      type: 'eventData',
+    },
+    {
+      title: t('pages.events.details.minAge'),
+      description: `${event.minAge} años`,
+      type: 'eventData',
+    },
+  ].filter(Boolean)
+
+  const eventServicesDetails = [
+    {
+      title: t('pages.events.details.price'),
+      description: `${event.price} €`,
+      type: 'eventData',
+    },
+    {
+      title: t('pages.events.details.allowCars'),
+      description: event.allowCars ? 'Sí' : 'No',
+      type: 'eventData',
+    },
+    {
+      title: t('pages.events.details.hasBar'),
+      description: event.hasBar ? 'Sí' : 'No',
+      type: 'eventData',
+    },
+  ].filter(Boolean)
+
+  const eventOtherDetails = [].filter(Boolean)
+
+  if (event.collaborators?.length) {
+    eventOtherDetails.push({
+      title: t('pages.events.details.collaborators'),
+      description: event.collaborators.join(', '),
+    })
+  }
 
   return (
-    <div className="container p-6 mx-auto">
-      <h1 className="text-3xl font-semibold text-center text-gray-800">
-        {event.title}
-      </h1>
+    <div className="px-4">
+      <h1 className="text-center t40s mb-28">{event.title}</h1>
 
-      {event.imageURL && (
-        <img
-          src={event.imageURL}
-          alt={event.title}
-          className="object-cover w-full h-64 my-6 rounded-lg shadow-md"
-        />
-      )}
-
-      <p className="mb-6 text-lg text-gray-700">{event.description}</p>
-
-      <div className="mb-4">
-        <p>
-          <strong>{t('pages.events.details.startDate')}:</strong>{' '}
-          {new Date(event.startDate).toLocaleString()}
-        </p>
-        {event.endDate && (
-          <p>
-            <strong>{t('pages.events.details.endDate')}:</strong>{' '}
-            {new Date(event.endDate).toLocaleString()}
-          </p>
-        )}
-      </div>
-
-      <div className="mb-4">
-        <p>
-          <strong>{t('pages.events.details.startTime')}:</strong>{' '}
-          {event.startTime}
-        </p>
-        {event.endTime && (
-          <p>
-            <strong>{t('pages.events.details.endTime')}:</strong>{' '}
-            {event.endTime}
-          </p>
-        )}
-      </div>
-
-      <p className="mb-4">
-        <strong>{t('pages.events.details.location')}:</strong> {event.location}
-      </p>
-
-      <div className="mb-4">
-        <p>
-          <strong>{t('pages.events.details.capacity')}:</strong>{' '}
-          {event.capacity}
-        </p>
-        <p>
-          <strong>{t('pages.events.details.price')}:</strong> {event.price} €{' '}
-        </p>
-        <p>
-          <strong>{t('pages.events.details.minAge')}:</strong> {event.minAge}{' '}
-          años
-        </p>
-      </div>
-
-      {event.collaborators && event.collaborators.length > 0 && (
-        <div className="mb-4">
-          <strong>{t('pages.events.details.collaborators')}:</strong>
-          <ul className="ml-6 list-disc">
-            {event.collaborators.map((collaborator, index) => (
-              <li key={index} className="text-gray-700">
-                {collaborator}
-              </li>
-            ))}
-          </ul>
+      <div className="grid gap-6 md:grid-cols-5">
+        <div className="md:col-span-3">
+          <DynamicCard
+            key={event.eventId}
+            type="gallery"
+            imageUrl={
+              event.imageURL
+                ? event.imageURL
+                : event.eventURL
+                  ? event.eventURL
+                  : '/placeholder.png'
+            }
+          />
         </div>
-      )}
-
-      {event.tags && event.tags.length > 0 && (
-        <div className="mb-4">
-          <strong>{t('pages.events.details.tags')}:</strong>
-          <ul className="flex space-x-4">
-            {event.tags.map((tag, index) => {
-              const tagColor = tagColors[tag] || 'bg-gray-600'
-              return (
-                <li
-                  key={index}
-                  className={`px-4 py-2 rounded-lg text-white ${tagColor}`}
-                >
-                  {tag}
-                </li>
-              )
-            })}
-          </ul>
+        <div>
+          <div className="space-y-4 bg-[#D9D9D9] rounded-[60px] h-fit w-[430px] mb-8">
+            <h3 className="pt-4 pl-8 t40b">Horas y fechas</h3>
+            <DynamicItems items={eventDataDetails || []} />
+          </div>
+          <div className="space-y-4 bg-[#D9D9D9] rounded-[60px] h-fit w-[430px] mb-8">
+            <h3 className="pt-4 pl-8 t40b">Localización y acceso </h3>
+            <DynamicItems items={eventAccessDetails || []} />
+          </div>
+          <div className="space-y-4 bg-[#D9D9D9] rounded-[60px] h-fit w-[430px] mb-8">
+            <h3 className="pt-4 pl-8 t40b">Precios y servicios</h3>
+            <DynamicItems items={eventServicesDetails || []} />
+          </div>
+          <button className="w-full p-3 text-white bg-black rounded-lg">
+            Get a ticket
+          </button>
         </div>
-      )}
+      </div>
 
-      <div className="mb-4">
-        <p>
-          <strong>{t('pages.events.details.organizer')}:</strong>{' '}
-          {event.organizer}
-        </p>
-        <p>
-          <strong>{t('pages.events.details.allowCars')}:</strong>{' '}
-          {event.allowCars ? 'Sí' : 'No'}
-        </p>
-        <p>
-          <strong>{t('pages.events.details.hasBar')}:</strong>{' '}
-          {event.hasBar ? 'Sí' : 'No'}
-        </p>
-        <p>
-          <strong>{t('pages.events.details.status')}:</strong> {event.status}
-        </p>
+      <div className="flex flex-col items-center justify-between p-4 bg-gray-200 rounded-lg md:flex-row">
+        <div>
+          <p className="font-bold">Organized by:</p>
+          <p>{event.organizer}</p>
+        </div>
+        <div>
+          <p className="font-bold">With the collaboration of:</p>
+          <p>{event.collaborators?.join(', ') || 'N/A'}</p>
+        </div>
+      </div>
+
+      <h2 className="text-2xl font-bold text-center">
+        With the performance of:
+      </h2>
+      <div className="space-y-6">
+        {event.performers?.map((performer, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center gap-6 md:flex-row"
+          >
+            <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded-lg md:w-1/3">
+              <span className="text-gray-500">Image Placeholder</span>
+            </div>
+            <div className="flex-1 text-center md:text-left">
+              <h3 className="text-xl font-bold">{performer.name}</h3>
+              <p className="text-gray-600">{performer.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
