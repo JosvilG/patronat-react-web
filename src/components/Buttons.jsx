f99f562241c126d14a2b57bf74545594b0a9ed27
JosvilG <josvilgprojects@gmail.com>
2025-03-21 23:06:15 +0100

    feat: New property (type) in DynamicButton

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import SaveIcon from '@mui/icons-material/Save'
+import CancelIcon from '@mui/icons-material/Cancel'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import LoopIcon from '@mui/icons-material/Loop'
+import SendIcon from '@mui/icons-material/Send'
 
-const DynamicButton = ({ size, state, textId, children, ...props }) => {
+const DynamicButton = ({ size, state, type, textId, children, ...props }) => {
   const { t } = useTranslation()
 
   const getSizeClasses = (size) => {
@@ -13,6 +22,8 @@ const DynamicButton = ({ size, state, textId, children, ...props }) => {
         return 'w-[250px] h-[56px]'
       case 'small':
         return 'w-[150px] h-[41px]'
+      case 'x-small':
+        return 'w-[41px] h-[41px]'
       default:
         return 'w-[250px]'
     }
@@ -23,7 +34,7 @@ const DynamicButton = ({ size, state, textId, children, ...props }) => {
       case 'normal':
         return 'bg-[#3A3A3A] text-[#FFFFFF] hover:bg-[#696969] hover:text-[#3A3A3A]'
       case 'disabled':
-        return 'bg-[rgba(91,91,91,0.53)] text-[rgba(80,79,80,0.31)]  cursor-not-allowed'
+        return 'bg-[rgba(91,91,91,0.53)] text-[rgba(80,79,80,0.31)] cursor-not-allowed'
       case 'highlighted':
         return 'bg-[#696969] text-[#3A3A3A] hover:bg-[#3A3A3A] hover:text-[#FFFFFF]'
       default:
@@ -31,22 +42,70 @@ const DynamicButton = ({ size, state, textId, children, ...props }) => {
     }
   }
 
+  const getTypeClasses = (type) => {
+    switch (type) {
+      case 'delete':
+        return {
+          icon: <DeleteIcon />,
+          classes:
+            'bg-[#EB0E00] text-[#FFFFFF] hover:bg-[#E55C52] hover:text-[#FFFFFF]',
+        }
+      case 'edit':
+        return {
+          icon: <EditIcon />,
+        }
+      case 'view':
+        return {
+          icon: <VisibilityIcon />,
+        }
+      case 'save':
+        return {
+          icon: <SaveIcon />,
+        }
+      case 'cancel':
+        return {
+          icon: <CancelIcon />,
+        }
+      case 'confirm':
+        return {
+          icon: <CheckCircleIcon />,
+        }
+      case 'add':
+        return {
+          icon: <AddCircleIcon />,
+        }
+      case 'loading':
+        return {
+          icon: <LoopIcon className="animate-spin" />,
+        }
+      case 'submit':
+        return {
+          icon: <SendIcon />,
+        }
+      default:
+        return { icon: null, classes: '' }
+    }
+  }
+
   const sizeClasses = getSizeClasses(size)
   const stateClasses = getStateClasses(state)
+  const { icon, classes: typeClasses } = getTypeClasses(type)
 
   return (
     <button
-      className={`t16s flex justify-center items-center ${sizeClasses} ${stateClasses} px-4 py-2 rounded-[60px] ${props.className}`}
+      className={`t16s flex justify-center items-center ${sizeClasses} ${stateClasses} px-4 py-2 rounded-[60px] ${props.className} ${typeClasses}`}
       disabled={state === 'disabled' || props.disabled}
       {...props}
     >
-      {textId ? t(textId) : children}
+      {textId ? t(textId) : icon || children || ''}
     </button>
   )
 }
+
 DynamicButton.propTypes = {
   size: PropTypes.string,
   state: PropTypes.string,
+  type: PropTypes.string,
   textId: PropTypes.string,
   children: PropTypes.node,
   className: PropTypes.string,
