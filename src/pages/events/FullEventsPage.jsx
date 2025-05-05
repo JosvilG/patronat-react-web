9737a2ce63a236d0a19726111a954afbe4c8bdff
JosvilG <josvilgprojects@gmail.com>
2025-05-05 21:34:25 +0200

    fix: link redirection in event list view when user is not admin

--- src/pages/events/FullEventsPage.jsx
+++ src/pages/events/FullEventsPage.jsx
@@ -4,10 +4,12 @@ import DynamicItems from '../../components/Items'
 import DynamicCard from '../../components/Cards'
 import Loader from '../../components/Loader'
 import { AuthContext } from '../../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 const FullEventsPage = () => {
   const { events, loading, error } = useEvents()
   const { userData } = useContext(AuthContext)
+  const navigate = useNavigate()
 
   if (loading) {
     return <Loader loading={loading} />
@@ -22,6 +24,10 @@ const FullEventsPage = () => {
   }
 
   const isAdmin = userData?.role === 'admin'
+
+  const handleEventClick = (event) => {
+    navigate(`/event/${event.title.toLowerCase().replace(/ /g, '-')}`)
+  }
 
   return (
     <div className="container min-h-screen p-4 mx-auto">
@@ -51,25 +57,30 @@ const FullEventsPage = () => {
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {events.map((event) => {
             return (
-              <DynamicCard
+              <div
                 key={event.eventId}
-                type="event"
-                title={event.title}
-                description={event.description}
-                date={new Date(event.start).toLocaleDateString('es-ES', {
-                  day: '2-digit',
-                  month: 'short',
-                  year: 'numeric',
-                })}
-                imageUrl={
-                  event.imageURL
-                    ? event.imageURL
-                    : event.eventURL
-                      ? event.eventURL
-                      : '/placeholder.png'
-                }
-                link={`/event/${event.title.toLowerCase().replace(/ /g, '-')}`}
-              />
+                onClick={() => handleEventClick(event)}
+                className="cursor-pointer"
+              >
+                <DynamicCard
+                  type="event"
+                  title={event.title}
+                  description={event.description}
+                  date={new Date(event.start).toLocaleDateString('es-ES', {
+                    day: '2-digit',
+                    month: 'short',
+                    year: 'numeric',
+                  })}
+                  imageUrl={
+                    event.imageURL
+                      ? event.imageURL
+                      : event.eventURL
+                        ? event.eventURL
+                        : '/placeholder.png'
+                  }
+                  link={`/event/${event.title.toLowerCase().replace(/ /g, '-')}`}
+                />
+              </div>
             )
           })}
         </div>
