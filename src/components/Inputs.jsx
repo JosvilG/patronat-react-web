57587841bb14b5d2e0b2837004a7e83ac9f03eaa
JosvilG <josvilgprojects@gmail.com>
2025-05-05 18:24:15 +0200

    style: inputs styles modified

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -27,13 +27,13 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
   const renderCustomSelect = () => (
     <div className="my-4">
-      <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+      <label htmlFor={name} className="block mb-2 t16r">
         {translatedLabel}
       </label>
       <div className="relative">
         {/* Custom Select box */}
         <div
-          className="w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] cursor-pointer flex justify-between items-center"
+          className="w-[400px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl cursor-pointer flex justify-between items-center"
           onClick={() => setIsOpen(!isOpen)}
         >
           <span className="text-[#696969]">
@@ -53,7 +53,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
               <div
                 key={index}
                 onClick={() => handleSelectOption(option)}
-                className="px-4 py-2 w-[348px] mb-1 text-[#D9D9D9] hover:bg-[#797979] bg-[#545151] rounded-[27px] cursor-pointer"
+                className="px-4 py-2 w-[348px] mb-1 text-[#D9D9D9] hover:bg-[#797979] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl"
               >
                 {t(option.label)}
               </div>
@@ -68,33 +68,38 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
     switch (type) {
       case 'text':
         return (
-          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2  min-w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]">
-            <input
-              name={name}
-              type="text"
-              placeholder={translatedPlaceholder}
-              className="t16l w-[312px] overflow-hidden bg-[#D9D9D9] text-[#696969] focus:outline-none"
-              inputProps={{
-                className:
-                  'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
-              }}
-              {...props}
-            />
-            <TitleIcon
-              fontSize="large"
-              className="relative left-4 max-sm:left-0 text-[#696969]"
-            />
+          <div>
+            <label htmlFor={name} className="block mb-2 t16r">
+              {translatedLabel}
+            </label>
+            <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2  min-w-[400px] h-[54px] px-4 py-2 flex flex-row items-center text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
+              <input
+                name={name}
+                type="text"
+                placeholder={translatedPlaceholder}
+                className="t16l w-[312px] overflow-hidden focus:outline-none bg-transparent"
+                inputProps={{
+                  className:
+                    'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2 ',
+                }}
+                {...props}
+              />
+              <TitleIcon
+                fontSize="large"
+                className="relative left-4 max-sm:left-0 text-[#696969]"
+              />
+            </div>
           </div>
         )
 
       case 'users':
         return (
-          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]">
+          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2  text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
             <input
               name={name}
               type="text"
               placeholder={translatedPlaceholder}
-              className="t16l w-[312px] overflow-hidden bg-[#D9D9D9] text-[#696969] focus:outline-none"
+              className="t16l w-[312px] overflow-hidden  focus:outline-none"
               inputProps={{
                 className:
                   'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
@@ -110,12 +115,12 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
       case 'email':
         return (
-          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]">
+          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
             <input
               name={name}
               type="email"
               placeholder={translatedPlaceholder}
-              className="t16l w-[312px] overflow-hidden bg-[#D9D9D9] text-[#696969] focus:outline-none"
+              className="t16l w-[312px] overflow-hidden  focus:outline-none"
               inputProps={{
                 className:
                   'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
@@ -131,12 +136,12 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
       case 'password':
         return (
-          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]">
+          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
             <input
               name={name}
               type="password"
               placeholder={t('components.inputs.enterPassword')}
-              className="t16l w-[312px] overflow-hidden bg-[#D9D9D9] text-[#696969] focus:outline-none"
+              className="t16l w-[312px] overflow-hidden  focus:outline-none"
               inputProps={{
                 className:
                   'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
@@ -152,14 +157,14 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       case 'number':
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <input
               name={name}
               type="number"
               placeholder={translatedPlaceholder}
-              className="t16l max-w-[200px] h-[54px] px-4 pr-12 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] focus:ring-2 appearance-none"
+              className="t16l max-w-[200px] h-[54px] px-4 pr-12 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl focus:ring-2 appearance-none"
               inputProps={{
                 className:
                   'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
@@ -172,12 +177,12 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
       case 'dni':
         return (
-          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]">
+          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
             <input
               name={name}
               type="text"
               placeholder={translatedPlaceholder}
-              className="t16l w-[312px] overflow-hidden bg-[#D9D9D9] text-[#696969] focus:outline-none"
+              className="t16l w-[312px] overflow-hidden  focus:outline-none"
               inputProps={{
                 className:
                   'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
@@ -193,12 +198,12 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
       case 'phone':
         return (
-          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]">
+          <div className="my-4 w-fit max-sm:min-w-[350px] max-sm:pr-2 min-w-[400px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
             <input
               name={name}
               type="number"
               placeholder={translatedPlaceholder}
-              className="t16l w-[312px] overflow-hidden bg-[#D9D9D9] text-[#696969] focus:outline-none"
+              className="t16l w-[312px] overflow-hidden  focus:outline-none"
               inputProps={{
                 className:
                   'focus:ring-2 focus:ring-blue-500 rounded-md px-4 py-2',
@@ -218,7 +223,10 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
             className="w-fit "
             onClick={() => document.getElementById(name)?.click()}
           >
-            <div className="flex items-center w-fit h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] cursor-pointer">
+            <label htmlFor={name} className="block mb-2 t16r">
+              {translatedLabel}
+            </label>
+            <div className="flex items-center w-fit h-[54px] px-4 py-2 border  t16r cursor-pointer  text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl">
               <input
                 type="checkbox"
                 id={name}
@@ -231,7 +239,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
               <label
                 htmlFor={name}
-                className="text-[#696969] peer-checked:text-white cursor-pointer select-none"
+                className="cursor-pointer select-none t16r peer-checked:text-white"
                 onClick={() => document.getElementById(name)?.click()}
               >
                 {translatedLabel}
@@ -245,7 +253,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
             className="w-fit "
             onClick={() => document.getElementById(name)?.click()}
           >
-            <div className="flex items-center w-fit h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] cursor-pointer">
+            <div className="flex items-center w-fit h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl cursor-pointer">
               <input
                 type="checkbox"
                 id={name}
@@ -258,7 +266,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
 
               <label
                 htmlFor={name}
-                className="text-[#696969] peer-checked:text-white cursor-pointer select-none"
+                className="cursor-pointer select-none t16r"
                 onClick={() => document.getElementById(name)?.click()}
               >
                 {translatedLabel}
@@ -269,13 +277,13 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       case 'textarea':
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <textarea
               name={name}
               placeholder={translatedPlaceholder}
-              className="t16l min-w-[400px] min-h-[54px] h-fit px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969]"
+              className="t16l min-w-[400px] w-full min-h-[54px] h-fit px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl"
               {...props}
             />
           </div>
@@ -287,13 +295,13 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       case 'date':
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <input
               type="date"
               name={name}
-              className="t16l w-[200px] max-w-[200px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] appearance-none"
+              className="t16l w-[200px] max-w-[200px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl appearance-none"
               {...props}
             />
           </div>
@@ -302,13 +310,13 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       case 'time':
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <input
               type="time"
               name={name}
-              className="t16l w-[200px] max-w-[200px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] appearance-none"
+              className="t16l w-[200px] max-w-[200px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl appearance-none"
               {...props}
             />
           </div>
@@ -317,7 +325,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       case 'otp':
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <div className="flex space-x-2">
@@ -327,7 +335,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
                   type="text"
                   maxLength="1"
                   name={`${name}[${index}]`}
-                  className="t24s max-w-[54px] h-[54px] px-4 py-2 border rounded-[24px] bg-[#D9D9D9] text-[#696969] text-center caret-transparent"
+                  className="t24s max-w-[54px] h-[54px] px-4 py-2   text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl text-center caret-transparent"
                   placeholder="X"
                   onInput={(e) => {
                     const target = e.target
@@ -354,25 +362,22 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       case 'document':
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <div className="w-[400px] h-[54px]">
               <label
                 htmlFor={name}
-                className="flex w-[400px] h-[54px] items-center px-4 py-2 text-[#696969] transition duration-200 bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300"
+                className="flex w-[400px] h-[54px] items-center px-4 py-2 t16r transition duration-200  cursor-pointer  text-[#696969] backdrop-blur-lg backdrop-saturate-[180%] bg-[rgba(255,255,255,0.75)] rounded-xl"
               >
                 <span
-                  className="mr-2 t16l max-w-[312px] w-[312px] overflow-hidden"
+                  className="mr-2 t16r max-w-[312px] w-[312px] overflow-hidden"
                   placeholder=""
                 >
                   {t('components.inputs.addDocument')}
                 </span>
                 <div className="relative right-0 flex items-center justify-center">
-                  <NoteAddIcon
-                    fontSize="large"
-                    className="text-[#696969]"
-                  ></NoteAddIcon>
+                  <NoteAddIcon fontSize="large" className="t16r"></NoteAddIcon>
                 </div>
               </label>
               <input id={name} type="file" className="hidden" {...props} />
@@ -383,7 +388,7 @@ const DynamicInput = ({ name, textId, type, options, ...props }) => {
       default:
         return (
           <div className="my-4">
-            <label htmlFor={name} className="block mb-2 text-sm font-semibold">
+            <label htmlFor={name} className="block mb-2 t16r">
               {translatedLabel}
             </label>
             <Input
